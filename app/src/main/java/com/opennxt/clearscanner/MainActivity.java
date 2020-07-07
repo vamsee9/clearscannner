0fc9fee1cd030d59a461aa28796e1d3927a7530c
vamsee9 <4n0n5ur7@protonmail.com>
2020-07-07 19:03:21 +0530

    created openCamera and openGallery [Non NDK Side by Side]
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- app/src/main/java/com/opennxt/clearscanner/MainActivity.java
+++ app/src/main/java/com/opennxt/clearscanner/MainActivity.java
@@ -2,8 +2,18 @@ package com.opennxt.clearscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCamera(View v)
     {
-
+        int REQUEST_CODE = 99;
+        int preference = ScanConstants.OPEN_CAMERA;
+        Intent intent = new Intent(this, ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
     public void openGallery(View v)
     {
+        int REQUEST_CODE = 99;
+        int preference = ScanConstants.OPEN_MEDIA;
+        Intent intent = new Intent(this, ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
 
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        float requestCode = 0;
+        if (requestCode == 99 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                getContentResolver().delete(uri, null, null);
+              //  scannedImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
