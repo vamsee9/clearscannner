205d1021a91cbf1797b8749a031a52baa012e4af
Claudemir Todo Bom <claudemir@todobom.com>
2020-08-10 11:38:34 -0300

    * create folder and save file inside it

--- scanlibrary/src/main/java/com/scanlibrary/Utils.java
+++ scanlibrary/src/main/java/com/scanlibrary/Utils.java
@@ -3,10 +3,12 @@ package com.scanlibrary;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,12 +24,43 @@ public class Utils {
 
     public static Uri getUri(Context context, Bitmap bitmap) {
 
-        String timeStamp = new SimpleDateFormat("dd-MM-YYYY HH.mm.ss").format(new
+        String timeStamp;
+        timeStamp = new SimpleDateFormat("dd-MM-YYYY HH.mm.ss").format(new
                 Date());
 
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "ClearScanner " + timeStamp, null);
+        // check existence of folder, if not creates it
+        String folderName = "clearscanner";
+        File folder = new File(Environment.getExternalStorageDirectory().toString()
+                , "/" + folderName);
+        if (!folder.exists()) {
+            if (!folder.mkdirs()) {
+                return null;
+            }
+        }
+        String fileName = "clearscanner" + timeStamp + ".jpg";
+
+        // save bitmap to file
+        // snippet took from https://stackoverflow.com/questions/45410883/how-to-save-images-in-an-specific-folder-android
+        File file = new File(new File(Environment.getExternalStorageDirectory() + "/" + folderName + "/"), fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // add created file to gallery roll
+        String path = null;
+        try {
+            path = MediaStore.Images.Media.insertImage(context.getContentResolver(), folder + "/" + fileName, "ClearScanner " + timeStamp, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return Uri.parse(path);
     }
 
